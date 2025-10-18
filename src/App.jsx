87fcfa2984efa38678ed1a463edde41f4b880380
Erin Telfer <eringtelfer@gmail.com>
2025-10-18 14:16:40 -0500

    completed adding buttons that move jokes between told and untold categories as well as delete after making it so jokes could be separated by told key value and displayed on the browser.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import { useEffect, useState } from "react"
-import { getAllJokes,postNewJoke } from "./services/jokeService"
+import { getAllJokes,postNewJoke,toggleJokeStatus,deleteJoke } from "./services/jokeService"
 import "./App.css"
 import stevePic from "./assets/steve.png"
 
 export const App = () => {
 
   const [newJoke, setNewJoke] = useState("")
+  const [allJokes, setAllJokes] = useState([])
+  const [untoldJokes, setUntoldJokes] = useState([])
+  const [toldJokes, setToldJokes] = useState([])
 
+const rerenderNewJoke = () => {
+  getAllJokes().then((jokeArr) => {
+  setAllJokes(jokeArr)
+})
+  }
 
 const handlePostJoke = async () => {
   await postNewJoke(newJoke)
   setNewJoke("")
+  rerenderNewJoke()
 }
+
+useEffect(() => {
+  getAllJokes().then((jokesArray) => {
+    setAllJokes(jokesArray)
+    console.log("jokes set!")
+  })
+}, [])
+
+  useEffect(() => { 
+    const toldJokesArray = allJokes.filter(joke => joke.told === true)
+    setToldJokes(toldJokesArray)
+
+    const untoldJokesArray = allJokes.filter(joke => joke.told === false)
+    setUntoldJokes(untoldJokesArray)
+  },[allJokes])
+
+const handleToggle = (joke) => {
+
+  toggleJokeStatus(joke).then(() => {
+    rerenderNewJoke()
+  })
+}
+
+const handleDelete = (joke) => {
+  deleteJoke(joke).then(() => {
+    rerenderNewJoke()
+  })
+}
+
+
 
 return ( 
 <div className="app-container">
@@ -35,6 +74,42 @@ return (
   onClick={handlePostJoke}>
     Add
   </button>
+</div>
+<div className="joke-lists-container">
+  <div className="joke-list-container">
+    <h2>
+      Untold Jokes
+      <span className="untold-count">{untoldJokes.length}</span>
+    </h2>
+    <ul>
+      {untoldJokes.map((joke) => {
+        return (
+          <li className="joke-list-item" key={joke.id}>
+            <p className="joke-item-text">{joke.text}</p>
+            <button className="joke-list-action-toggle" onClick={() => handleToggle(joke)}><i className="fa-regular fa-face-smile" /></button>
+            <button className="joke-list-action-delete" onClick={() => handleDelete(joke)}><i className="fa-regular fa-trash-can" /></button>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+    <div className="joke-list-container">
+    <h2>
+      Told Jokes
+      <span className="told-count">{toldJokes.length}</span>
+    </h2>
+    <ul>
+      {toldJokes.map((joke) => {
+        return (
+          <li className="joke-list-item" key={joke.id}>
+            <p className="joke-item-text">{joke.text}</p>
+            <button className="joke-list-action-toggle" onClick={() => handleToggle(joke)}><i className="fa-regular fa-face-meh" /></button>
+            <button className="joke-list-action-delete" onClick={() => handleDelete(joke)}><i className="fa-regular fa-trash-can" /></button>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
 </div>
 </div>
 
--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -16,3 +16,29 @@ export const postNewJoke = async (jokeText) => {
         }
         await fetch("http://localhost:8088/jokes", postOptions)
     }
+
+
+export const toggleJokeStatus = async (joke) => {
+  const editedJoke = {
+    id: joke.id,
+    text: joke.text,
+    told: !joke.told,
+  }
+
+  await fetch(`http://localhost:8088/jokes/${joke.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(editedJoke),
+  })
+}
+
+
+export const deleteJoke = async (joke) => {
+  const deleteOptions = {
+    method: "DELETE",
+  }
+
+  await fetch(`http://localhost:8088/jokes/${joke.id}`, deleteOptions);
+};
